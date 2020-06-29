8529a4cd31320e63a8bd670df5281821a165938e
Flavio <flavio.alessandri@gmail.com>
2020-06-29 16:16:11 +0200

    JS - palindrome

--- js/script.js
+++ js/script.js
@@ -1,25 +1,42 @@
 console.log("Scopri se la parola digitata è palindroma");
 
+// var btn = document.getElementById("btn");
+// btn.addEventListener("click", function(){
+//
+//   var name = document.getElementById("name").value;
+//   var letters=[];
+//
+//
+//   for (i=name.length-1; i>= 0; i--){
+//   letters +=(name[i]);
+//   console.log(letters);
+//   }
+//
+//   if(name === letters){
+//     document.getElementById("result").innerHTML="PALINDROMO";
+//   }else{
+//         document.getElementById("result").innerHTML="no";
+//   }
+//
+// })
 
-var btn = document.getElementById("btn");
+var btn = document.getElementById("btn"); //call the button
 
+btn.addEventListener("click", function (){ //after button click
+var name = document.getElementById("name").value; //get name value
+name = name.toUpperCase();
+if (name === stringIsReverseString(name)){
+  document.getElementById("result").innerHTML="PALINDROME";
 
-
-btn.addEventListener("click", function(){
-
-  var name = document.getElementById("name").value;
-  var letters=[];
-
-
-  for (i=name.length-1; i>= 0; i--){
-  letters +=(name[i]);
-  console.log(letters);
-  }
-
-  if(name === letters){
-    document.getElementById("result").innerHTML="PALINDROMO";
-  }else{
-        document.getElementById("result").innerHTML="no";
-  }
+} else{
+  document.getElementById("result").innerHTML="Not palindrome";
+}
 
 })
+
+function stringIsReverseString(array){
+  var newArray=[];
+  for (i=array.length-1; i>= 0; i--){
+  newArray +=(array[i]);
+  }return newArray;
+}
